74aaf98cdfd6efed1e0565167dce9b4c72f4b903
Nataliia Kononikhina <kononikhinan@gmail.com>
2017-10-04 15:03:44 +0200

    Добавляю стили форме

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
-var open = document.querySelector(".best-product__order");
+var openInIndex = document.querySelector(".best-product__order");
+var openInCatalog = document.querySelector(".product__odrer");
 var popup = document.querySelector(".order-wrapper");
-var closePopup = document.querySelector("order__add");
+var closePopup = document.querySelector(".order__add");
 
-open.addEventListener("click", function (evt) {
+if (openInIndex) {
+  openInIndex.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("show-popup");
+  });
+}
+
+openInCatalog.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("show-popup");
 });
